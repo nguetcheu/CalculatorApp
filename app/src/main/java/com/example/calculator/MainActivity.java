26096c953a896c1426ddd073d56815ba5a57146f
nguetcheu <dnguetcheu@gmail.com>
2024-11-14 23:44:05 +0100

    refactorisation possible

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -76,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
         button9.setOnClickListener(listener);
         buttonDot.setOnClickListener(listener);
 
-        /* 
+        /*
         int[] buttonIds = {R.id.button0, R.id.button1, R.id.button2, R.id.button3, R.id.button4,
                 R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9,
                 R.id.buttonDot};
